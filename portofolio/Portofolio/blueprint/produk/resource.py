88e117fd6f004a8a4bed962d51efc7a9634124d0
Rizky Dwi Hidayat <rizky@alphatech.id>
2019-03-18 20:41:18 +0700

    pisah

--- portofolio/Portofolio/blueprint/produk/resource.py
+++ portofolio/Portofolio/blueprint/produk/resource.py
@@ -30,12 +30,14 @@ class ProdukResource(Resource):
             parse.add_argument('nama_produk', location='json', required=True)
             parse.add_argument('kategori', location='json', required=True)
             parse.add_argument('harga', location='json', required=True)
+            parse.add_argument('warna', location='json', required=True)
+            parse.add_argument('ukuran', location='json', required=True)
             parse.add_argument('deskripsi', location='json', required=True)
             # parse.add_argument('role', location='json', default='customer')
 
             args = parse.parse_args()
 
-            produks = Produk(None, args['nama_produk'],args['kategori'], args['harga'], args['deskripsi'])
+            produks = Produk(None, args['nama_produk'],args['kategori'], args['harga'], args['warna'], args['ukuran'], args['deskripsi'])
             db.session.add(produks)
             db.session.commit()
 
@@ -43,35 +45,39 @@ class ProdukResource(Resource):
         # return {'status': 'UNAUTHORIZED', 'message': 'invalid role'}, 401
     
     @jwt_required
-    def put(self):
+    def put(self, produk_id):
         produks = get_jwt_claims()
 
         parse =reqparse.RequestParser()
         parse.add_argument('nama_produk', location='json', required=True)
         parse.add_argument('kategori', location='json', required=True)
         parse.add_argument('harga', location='json', required=True)
+        parse.add_argument('warna', location='json', required=True)
+        parse.add_argument('ukuran', location='json', required=True)
         parse.add_argument('deskripsi', location='json', required=True)
 
         args = parse.parse_args()
 
 
-        qry = Produk.query.get(customer['produk_id'])
+        qry = Produk.query.get(produk_id)
 
         qry.nama_produk = args['nama_produk']
         qry.kategori = args['kategori']
         qry.harga = args['harga']
+        qry.warna = args['warna']
+        qry.ukuran = args['ukuran']
         qry.deskripsi = args['deskripsi']
         db.session.commit()
         return {'message': 'Data diperbarui...', 'input': marshal(qry, Produk.response_fields)}, 200, {'Content-Type': 'application/json'}
 
     @jwt_required
-    def delete(self):#, usernamePenbeli):
+    def delete(self, produk_id):#, usernamePenbeli):
         produks = get_jwt_claims()
-        qry = Produk.query.get(produks['produk_id'])
+        qry = Produk.query.get(produk_id)
 
         db.session.delete(qry)
         db.session.commit()
         return {'message': 'Data sudah dihapus...'}, 200, {'Content-Type': 'application/json'}
 
 
-api.add_resource(ProdukResource, '/produk')
+api.add_resource(ProdukResource, '/produk', '/produk/<int:produk_id>')
