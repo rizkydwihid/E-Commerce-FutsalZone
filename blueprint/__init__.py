5c4c13da7859e51e5547d5c49097e37f56eeb6fd
Rizky Dwi Hidayat <rizky@alphatech.id>
2019-03-27 19:40:57 +0700

    database

--- blueprint/__init__.py
+++ blueprint/__init__.py
@@ -13,7 +13,7 @@ from flask_jwt_extended import JWTManager
 # initiate flask-restful instance
 app = Flask(__name__)
 # database config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@3.1.96.237:3306/portofolio'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/portofolio'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['JWT_SECRET_KEY'] = 'SFsieaaBsLEpecP675r243faM8oSB2hV'
